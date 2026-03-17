3a18e1e5da16e72201347932dd0caead2cbef547
Ismael Oliveira <ismael.oliveira@bitgrow.com.br>
2026-03-17 00:31:53 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,17 @@
-import 'dotenv/config'; 
-import { ValidationPipe } from '@nestjs/common';
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Filtro global para tratamento de erros
-  const { AllExceptionsFilter } = await import(
-    './filters/all-exceptions.filter'
-  );
-  app.useGlobalFilters(new AllExceptionsFilter());
 
-  // 🔥 Validação global
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: false,
-      transform: true,
-    }),
-  );
-
-  // 🔓 CORS —
+  // Habilitar CORS para o frontend da Vercel
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://javascript-full-stack-front-end.vercel.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false,
+    origin: ['https://projeto-encurtador-url-frontend-m6e.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // se você precisar enviar cookies
   });
 
-  const PORT = process.env.PORT || 3001;
-
-  await app.listen(PORT, () => {
-    console.log(`🚀 Backend rodando na porta ${PORT}`);
-  });
+  await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
